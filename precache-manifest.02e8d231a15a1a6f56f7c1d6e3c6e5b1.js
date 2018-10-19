d8a758e8058dc9ef47afac35dc1916d5b74d521a
tomkim825 <tk825@hotmail.com>
2018-10-18 23:43:00 -0700

    Updates

---
+++ precache-manifest.02e8d231a15a1a6f56f7c1d6e3c6e5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8463515c1d60e4ee1d57",
+    "url": "/testing-repo/static/js/runtime~main.8463515c.js"
+  },
+  {
+    "revision": "928ca56bc234cbc524b1",
+    "url": "/testing-repo/static/js/main.928ca56b.chunk.js"
+  },
+  {
+    "revision": "06117792dc4c67b5f6cf",
+    "url": "/testing-repo/static/js/1.06117792.chunk.js"
+  },
+  {
+    "revision": "928ca56bc234cbc524b1",
+    "url": "/testing-repo/static/css/main.82b87ab8.chunk.css"
+  },
+  {
+    "revision": "c56912c82f7fc852a0db8e699d83beea",
+    "url": "/testing-repo/index.html"
+  }
+];
